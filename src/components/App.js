4a9f487738de93313a6baed73f85158f8dd5f65f
Asif Mushtaq <asif.mushtaq@dpskw.com>
2024-08-13 13:44:29 +0500

    small change

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,51 @@
-import React,{useState,useEffect} from 'react';
-import SearchBar from './SearchBar';
-import youtube,{ baseParams } from '../apis/youtube';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import youtube, { baseParams } from "../apis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 const App = () => {
- 
-  const [videos,setVideos] = useState([]);
-  const [selectedVideo,setSelectedVideo] = useState([])
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState([]);
 
-  useEffect( () => {
-    onTermSubmit('buildings');
-  },[])
+  useEffect(() => {
+    onTermSubmit("buildings");
+  }, []);
 
   const onTermSubmit = async (term) => {
-    youtube.get('/search',{
-      params : {
-        ...baseParams,
-        q : term
-      }
-    }).then(res => {
-      setVideos(res.data.items);
-      console.log(res)
-      setSelectedVideo(res.data.items[0]);
-    }).catch(err => console.log(err))
+    youtube
+      .get("/search", {
+        params: {
+          ...baseParams,
+          q: term,
+        },
+      })
+      .then((res) => {
+        setVideos(res.data.items);
+        console.log("API RESPONSE", res);
+        setSelectedVideo(res.data.items[0]);
+      })
+      .catch((err) => console.log(err));
   };
 
-  const onVideoSelect = video => {
+  const onVideoSelect = (video) => {
     setSelectedVideo(video);
   };
   return (
-    <div  className = "ui container">
-     <SearchBar onFormSubmit={onTermSubmit}/>
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />
       <div className="ui grid">
         <div className="ui row">
-          <div className = "eleven wide column">
-            <VideoDetail video = {selectedVideo}/>
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
           </div>
-          <div className = "five wide column">
-            <VideoList videosList = {videos} onVideoSelect = {onVideoSelect}/>
-          </div> 
-        </div>  
-      </div>  
+          <div className="five wide column">
+            <VideoList videosList={videos} onVideoSelect={onVideoSelect} />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
